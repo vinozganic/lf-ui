fcc5bc3f2ef1ce85f8e6d1f4e488036e3912dae2
Teo Radolovict <tr54427@fer.hr>
2023-02-16 22:22:47 +0100

    Added Text components

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import SmallText from "./SmallText";
+import MediumText from "./MediumText";
+import BigText from "./BigText";
+import VeryBigText from "./VeryBigText";
+import Button from "react";
+
+export {
+    SmallText,
+    MediumText,
+    BigText,
+    VeryBigText,
+    Button,
+}
