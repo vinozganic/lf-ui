e3db8bd48a47f7292d5adc8d8f2577a0d6943fb1
Teo Radolovict <tr54427@fer.hr>
2023-02-16 22:42:20 +0100

    Changed index.js

--- src/components/BigText.jsx
+++ src/components/BigText.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const BigText = ({ children, className }) => {
-    return <div className={`text-5xl font-bold  ${className}`}>{children}</div>
+    return <div className={`text-5xl font-bold ${className}`}>{children}</div>
 }
 
 export default BigText
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import SmallText from "./SmallText";
 import MediumText from "./MediumText";
 import BigText from "./BigText";
 import VeryBigText from "./VeryBigText";
-import Button from "react";
+import Button from "./Button";
 
 export {
     SmallText,
