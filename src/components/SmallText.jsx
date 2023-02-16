3e5e5c7fc360786c3fed77e3d82ddb0b3656c46c
Marko <marko.bolt4@gmail.com>
2023-02-16 21:34:06 +0100

    added app skeleton

---
+++ src/components/SmallText.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const SmallText = ({ children }) => {
+    return <div className="text-lg font-bold">{children}</div>
+}
+
+export default SmallText
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+        colors: {
+            primary: "#E4C1F9",
+            secondary: "#FCF6BD",
+            background: "#D0F4DE",
+        },
+    },
+    plugins: [],
 }
