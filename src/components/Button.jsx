fda041f52a72daa399280b37d5c1c54baa1a489a
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2023-02-17 18:12:01 +0100

    Modified button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,12 +6,13 @@ const Button = ({ children, onClick, className }) => {
             <button
                 onClick={onClick}
                 type="button"
-                className="text-center font-bold px-8 py-3 border-solid border-black 
-                            border-opacity-30 border-2 border-b-4 bg-gradient-to-tr from-primary to-pink rounded-2xl
-                             hover:bg-gradient-to-tr hover:from-secondary hover:to-pink
-                            shadow-primary shadow-2xl hover:shadow-secondary text-black text-opacity-60
-                             active:px-8 active:py-3 active:ring-primary active:ring-1
-                             active:bg-gradient-to-tr active:from-secondary active:to-primary">
+                className="text-center transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100
+                                font-bold px-8 py-2 border-solid border-black w-full 
+                                border-opacity-30 border-2 border-b-4 bg-gradient-to-tr from-primary to-pink rounded-3xl
+                                hover:from-secondary hover:to-pink text-black
+                                shadow-primary shadow-2xl hover:shadow-secondary text-opacity-60
+                                active:ring-primary active:ring-1
+                                active:from-secondary active:to-primary active:shadow-sm">
                 {children}
             </button>
         </div>
