2d48f00a4cc3ec5ee7b3a9d6143c35167bcbb094
Marko <marko.bolt4@gmail.com>
2023-02-17 18:30:42 +0100

    added props propagation to Page component

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Page = ({ children, className }) => {
-    return <div className="p-6 h-screen bg-background">{children}</div>
+    return <div className={`p-6 h-screen bg-background ${className}`}>{children}</div>
 }
 
 export default Page
