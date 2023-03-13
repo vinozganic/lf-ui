929d7035638bac6eadca133b224c39cb66111514
Marko <marko.bolt4@gmail.com>
2023-03-13 23:56:27 +0100

    fixed according to pr comments

--- src/components/forms/ProgressBar.jsx
+++ src/components/forms/ProgressBar.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 const ProgressBar = ({ progress }) => {
     return (
-        <div className="flex fixed top-12 p-12 pb-24 w-full max-w-3xl bg-gradient-to-b from-background via-background to-transparent duration-300 ease-in-out pointer-events-none">
+        <div className="flex fixed top-12 p-12 pb-24 w-full max-w-7xl bg-gradient-to-b from-background via-background to-transparent duration-300 ease-in-out pointer-events-none">
             <div className="bg-gray w-full flex justify-center rounded-full pointer-events-auto">
                 <div className="w-full flex">
                     <div
                         style={{ width: `${progress}%` }}
-                        className={`transition-width ease rounded-full p-4 bg-white bg-gradient-to-r from-primaryLowOpacity to-primary background-animate drop-shadow-lg`}></div>
+                        className="transition-width ease rounded-full p-4 bg-white bg-gradient-to-r from-primaryLowOpacity to-primary background-animate drop-shadow-lg"></div>
                 </div>
             </div>
         </div>
