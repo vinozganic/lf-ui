a7eb1807f68cec82ca697389ae3b2ea5133d04dd
Marko <marko.bolt4@gmail.com>
2023-03-13 23:34:13 +0100

    started location component

---
+++ src/components/forms/index.js
@@ -0,0 +1,9 @@
+import addColourPicker from "./ColourPicker"
+import addLocationSelectQuestion from "./LocationSelect"
+import addMultipleChoiceQuestion from "./MultipleChoice"
+import addSingleChoiceQuestion from "./SingleChoice"
+import Question from "./Question"
+import Form from "./Form"
+
+export { addColourPicker, addLocationSelectQuestion, addMultipleChoiceQuestion, addSingleChoiceQuestion, Question }
+export default Form
