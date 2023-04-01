0eda96a03661e81d4a3c9c6c0a253f7909759e54
Teo Radolovict <tr54427@fer.hr>
2023-04-01 10:30:59 +0200

    LFZ-110 - Cleaner code and same style

--- src/components/MatchesList.jsx
+++ src/components/MatchesList.jsx
@@ -3,84 +3,71 @@ import MatchCard from "./MatchCard"
 import Modal from "./Modal"
 
 const MatchesList = ({ matches, lostItem }) => {
-    const [isVisible, setIsVisible] = useState(false)
-    const [displayMatch, setDisplayMatch] = useState({})
     const [matchesList, setMatchesList] = useState (
-        matches.map((match, index) => (
-            {...match, showProps: false, functionPassID: index, discarded: false }
+        matches.map((match) => (
+            { data: match, showProps: false, discarded: false }
         ))
-        .sort((a, b) => b.match_probability - a.match_probability)
+        .sort((a, b) => b.data.match_probability - a.data.match_probability)
       )
 
     const handleShowProps = (index) => {
-        const newSelectMathces = matchesList.map((match) => {
-            if (index === match.functionPassID) {
-                setDisplayMatch(match)
-                return { ...match, showProps: true }
+        const newSelectMatches = matchesList.map((match) => {
+            if (index === match.data.id) {
+                return { ...match, showProps: !match.showProps }
             }
             return { ...match, showProps: false }
         })
-        setMatchesList(newSelectMathces)
-        setIsVisible(true)
+        setMatchesList(newSelectMatches)
     }
 
     const handleCloseModal = () => {
-        setIsVisible(false)
-        const newMathces = matchesList.map((match) => {
+        const newMatches = matchesList.map((match) => {
             return { ...match, showProps: false }
         })
-        setMatchesList(newMathces)
-        document.body.style.overflow = ''
+        setMatchesList(newMatches)
     }
 
     const handleDiscard = (index) => {
         const newDiscardedMathces = matchesList.map((match) => {
-            if (index === match.functionPassID) {
-                const newDiscarded = !match.discarded
-                if (newDiscarded) {
-                    setIsVisible(false)
-                    return { ...match, showProps: false, discarded: newDiscarded }    
+            if (index === match.data.id) {
+                if (match.discarded) {
+                    return { ...match, discarded: !match.discarded }
                 }
-                return { ...match, discarded: newDiscarded }
+                return { ...match, showProps: false, discarded: !match.discarded }
             }
-            return { ...match, discarded: match.discarded }
+            return { ...match }
         }).sort((a, b) => {
             if (a.discarded && !b.discarded) {
               return 1;
             } else if (!a.discarded && b.discarded) {
               return -1;
             } else {
-              return b.match_probability - a.match_probability;
+              return b.data.match_probability - a.data.match_probability;
             }
         })
         setMatchesList(newDiscardedMathces)
     }
 
-    const lockScrollModal = () => {
-        document.body.style.overflow = 'hidden'
-    }
-
     const sortedListMatchCards = matchesList.map( (match, index) => (
             <li key={index}>
-                <MatchCard probability={match.match_probability} 
-                    matchId={match.id} 
-                    functionPassID={match.functionPassID} 
-                    lostItem={lostItem} 
-                    showProps={match.showProps}
+                <MatchCard
+                    match={match}
+                    lostItem={lostItem}
                     handleShowProps={handleShowProps}
                     handleDiscard={handleDiscard} 
-                    discarded={match.discarded}
-                    lockScrollModal={lockScrollModal}
-                    />
+                />
             </li>
         ))
 
     return (
-        <div className="relative flex max-md:my-0 md:my-6 lg:my-10 xl:my-14 max-lg:mx-0 lg:mx-10 xl:mx-40 max-lg:justify-center">
-            <ul className="relative grid sm:gap-y-20 max-sm:gap-y-10 w-fit">
+        <div className="flex my-10 lg:mx-10 xl:mx-40 max-lg:justify-center">
+            <ul className="grid sm:gap-y-20 gap-y-10">
                 {sortedListMatchCards}
             </ul>
-            <Modal isVisible={isVisible} onClose={handleCloseModal} displayMatch={displayMatch} />
+            <Modal 
+                onClose={handleCloseModal}
+                displayMatch={matchesList.find(match => match.showProps === true)?.data || {}} 
+            />
         </div>
     )
 }
