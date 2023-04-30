8baec2b299c51e43ce742dc96afa47dd6293dd5d
Fran <fran.sipic@gmail.com>
2023-04-30 18:19:49 +0200

    added color preview and button for color selection on color picker

--- src/components/forms/ColorPicker.jsx
+++ src/components/forms/ColorPicker.jsx
@@ -2,36 +2,56 @@ import React, { useState } from "react"
 import { ChromePicker } from "react-color"
 import Question from "./Question"
 import "./ColorPickerStyles.css"
+import { SmallText } from "../"
 
 const ColorPicker = ({ questionId, updateAnswer }) => {
-    const [color, setColor] = useState("#000000")
+    const [color, setColor] = useState("#15bfe6")
 
     const handleChangeComplete = (color) => {
         setColor(color.rgb)
-        updateAnswer([color.rgb.r, color.rgb.g, color.rgb.b], questionId)
+    }
+
+    const handleSelectColor = () => {
+        updateAnswer([color.r, color.g, color.b], questionId)
     }
 
     return (
-        <ChromePicker
-            color={color}
-            onChange={handleChangeComplete}
-            disableAlpha={true}
-            className="pb-2"
-            styles={{
-                default: {
-                    picker: {
-                        boxShadow: "none",
-                        border: "none",
-                        backgroundColor: "#384866",
+        <div className="flex">
+            <ChromePicker
+                color={color}
+                onChange={handleChangeComplete}
+                disableAlpha={true}
+                className="pb-2"
+                styles={{
+                    default: {
+                        picker: {
+                            boxShadow: "none",
+                            border: "none",
+                            backgroundColor: "#384866",
+                            borderRadius: "0.5rem",
+                        },
+                        saturation: {
+                            borderTopLeftRadius: "0.5rem",
+                            borderTopRightRadius: "0.5rem",
+                        },
+                    },
+                }}                
+            />
+            <div className="ml-5">
+                <div
+                    className="w-[150px] h-[123.75px] px-4 py-2 border-2 border-gray bg-primary"
+                    style={{
+                        backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})`,
+                        marginBottom: "1rem",
                         borderRadius: "0.5rem",
-                    },
-                    saturation: {
-                        borderTopLeftRadius: "0.5rem",
-                        borderTopRightRadius: "0.5rem",
-                    },
-                },
-            }}
-        />
+                    }}
+                ></div>
+                <div className="w-full text-center bg-gray px-4 py-2 
+                border-gray border-2 hover:bg-opacity-80 hover:border-primary hover:border-opacity-40 rounded-xl duration-100 cursor-pointer" onClick={handleSelectColor}>
+                    <SmallText className="select-none">Odaberi boju</SmallText>
+                </div>
+            </div>
+        </div>
     )
 }
 
