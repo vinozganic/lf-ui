76758ea060d9ff893fe73c96ee75445515d29080
Teo Radolovict <tr54427@fer.hr>
2023-05-06 00:49:01 +0200

    [LFZ-131] - deleted comments

--- src/components/forms/LocationSelect.jsx
+++ src/components/forms/LocationSelect.jsx
@@ -321,28 +321,6 @@ const RenderTypeList = ({ separateLines, typeShownID, handletypeShownID }) => {
         )
     })
 
-    // return (
-    //     <nav className="flex gap-x-2 w-full mb-2">
-    //         <span className={`border-2 border-gray rounded-s-xl cursor-pointer 
-    //             flex items-center min-h-full w-fit
-    //             sm:hover:bg-primary/40 transition-all ease-in-out duration-150`}
-    //             onClick={() => slideLeft()}>
-    //             <svg className="rotate-180 fill-white w-6" viewBox="0 0 20 20">
-    //                 <path d="M11.611,10.049l-4.76-4.873c-0.303-0.31-0.297-0.804,0.012-1.105c0.309-0.304,0.803-0.293,1.105,0.012l5.306,5.433c0.304,0.31,0.296,0.805-0.012,1.105L7.83,15.928c-0.152,0.148-0.35,0.223-0.547,0.223c-0.203,0-0.406-0.08-0.559-0.236c-0.303-0.309-0.295-0.803,0.012-1.104L11.611,10.049z"></path>
-    //             </svg>
-    //         </span>
-    //         <div ref={slider} className={`flex w-full snap-x snap-mandatory scroll-smooth gap-x-4 overflow-x-auto scrollbar-hide`}>
-    //             {renderTypeList}
-    //         </div>
-    //         <span className={`border-2 border-gray rounded-e-xl cursor-pointer min-h-full w-fit flex items-center
-    //             hover:bg-primary/40 transition-all ease-in-out duration-150`}
-    //             onClick={() => slideRight()}>
-    //             <svg className="fill-white w-6" viewBox="0 0 20 20">
-	//                 <path d="M11.611,10.049l-4.76-4.873c-0.303-0.31-0.297-0.804,0.012-1.105c0.309-0.304,0.803-0.293,1.105,0.012l5.306,5.433c0.304,0.31,0.296,0.805-0.012,1.105L7.83,15.928c-0.152,0.148-0.35,0.223-0.547,0.223c-0.203,0-0.406-0.08-0.559-0.236c-0.303-0.309-0.295-0.803,0.012-1.104L11.611,10.049z"></path>
-    //             </svg>
-    //         </span>
-    //     </nav>
-    // )
     return (
         <div className="flex gap-x-2 w-full mb-2">
             <span className={`border-2 border-gray rounded-s-xl cursor-pointer min-h-full w-fit flex items-center
